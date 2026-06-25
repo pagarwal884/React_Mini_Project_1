598d33751502931aa6089ce0180ac850c29ccf7f
pagarwal884 <pagarwal1145@gmail.com>
2026-06-25 23:53:14 +0530

    Make a component on page in which i have created input field first column

--- Hooks_Utilization_Learning/src/App.jsx
+++ Hooks_Utilization_Learning/src/App.jsx
@@ -1,6 +1,17 @@
+import InputBox from "./components/inputBox";
 
 function App() {
-
+  return (
+    <div
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      style={{
+        backgroundImage:
+          "url('https://images.pexels.com/photos/35251427/pexels-photo-35251427.jpeg')",
+      }}
+    >
+      <InputBox />
+    </div>
+  );
 }
 
-export default App
+export default App;
